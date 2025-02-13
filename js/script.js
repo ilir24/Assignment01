1a81b64a42225cb5e01e3b1f7e0fdee204d37fd3
Ilir Berisha <ilirberisha25@gmail.com>
2025-02-12 20:34:20 -0800

    Assignment01 start

---
+++ js/script.js
@@ -0,0 +1,31 @@
+//STEP1
+let some_Month;
+function the_Month();
+let current_Month;
+let summer_Month;
+let myLibrary_Function;
+//STEP2
+let thisIsANumber = 1;
+let thisIsAString = 'Hello';
+let thisIsABoolean = true;
+let x;
+//STEP3
+//STEP4
+//STEP5
+//STEP6
+//STEP7
+//STEP8
+//STEP9
+//STEP10
+//STEP11
+//STEP12
+//STEP13
+//STEP14
+//STEP15
+//STEP16
+//STEP17
+//STEP18
+//STEP19
+//STEP20
+//STEP21
+//STEP22
