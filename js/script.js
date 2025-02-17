a1a26598dc68c41a0273a80755202be77477f6da
Ilir Berisha <ilirberisha25@gmail.com>
2025-02-17 02:54:28 -0800

    Assignment 1 Completed

--- js/script.js
+++ js/script.js
@@ -1,31 +1,92 @@
 //STEP1
-let some_Month;
-function the_Month();
-let current_Month;
-let summer_Month;
-let myLibrary_Function;
+let someMonth;
+function theMonth();
+let currentMonth;
+let summerMonth;
+let myLibraryFunction;
 //STEP2
 let thisIsANumber = 1;
 let thisIsAString = 'Hello';
 let thisIsABoolean = true;
-let x;
+let thisIsANull = null;
 //STEP3
+let complexExpression = 20 + 20;
+let complexExpression2 = 20 - 20;
 //STEP4
+let firstName;
+let lastName;
+let address;
+let city;
+let state;
+let zipCode;
+let yourAge;
+let referralSource;
+let mayWeContactYou;
 //STEP5
+firstName = 'Ilir';
+lastName = 'Berisha';
+address = '200 Grapevine St';
 //STEP6
+let numberAndString;
+numberAndString = 1 + 'Hello';
+console.log(numberAndString);
 //STEP7
+let booleanAndString;
+let addNumberAndBoolean;
+booleanAndString = true + 'Hello';
+console.log(booleanAndString);
+addNumberAndBoolean = 1 + true;
+console.log(addNumberAndBoolean);
 //STEP8
+//The string literal is not valid because the quotation marks are not escaped with a backslash or the string is not enclosed in single or double quotes.
+let someString = "Who once said, 'Only two things are infinite, the universe and human stupidity, and I'm not sure about the former.'";
+console.log(someString);
 //STEP9
+let nullValue = null;
+console.log(nullValue);
+let undefinedValue;
+console.log(undefinedValue);
 //STEP10
+console.log(typeof 'Hello');
+console.log(typeof 1);
+console.log(typeof true);
+console.log(typeof null);
+console.log(typeof undefined);
 //STEP11
+alert ('Hello ' + 'Ilir Berisha' + ', welcome to the JavaScript class!');
 //STEP12
+let myName = 'Ilir Berisha';
+alert ('Hello ' + myName + ', welcome to the JavaScript class!');
 //STEP13
+let course = 'JavaScript';
+alert ('Hello ' + myName + ', welcome to the ' + course + ' class!');
 //STEP14
+alert ('Hello ' + myName + '.\nWelcome to the ' + course + ' class!');
 //STEP15
+myName = prompt('What is your name?');
 //STEP16
+course = prompt('What class are you taking?');
 //STEP17
+let x = 10;
+let y = 20;
+console.log(x + y);
 //STEP18
+x = 20;
+x += 20;
+console.log(x);
 //STEP19
+x = 20;
+x *= 5;
+console.log(x);
 //STEP20
+x = 20 % 3;
+x /= 1;
+console.log(x);
 //STEP21
+let firstNumber = 5;
+let secondNumber = 10;
+console.log(firstNumber < secondNumber);
 //STEP22
+let thirdNumber = 10;
+let fourthNumber = 20;
+console.log(thirdNumber === fourthNumber);
